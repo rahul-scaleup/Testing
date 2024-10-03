b9b3923a78804467ef85f033170ca3013b3182d0
Rahul Kumar <rahul@scaleupapp.club>
2024-10-03 13:07:00 +0530

    Save all changes

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hey how are you !");
+
+let a = 10;
+let b = 20;
+
+function sum(a, b) {
+    return a+b;
+}
+
+console.log(sum(a+b));
+console.log("All changes are saved");
+console.log('adding somr changes');
